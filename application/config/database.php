e31aa2d3c9c67a8425b3a335f524f2122113e651
ivansf <ivansotof@gmail.com>
2011-07-18 22:34:46 -0600

    Revert "getting rid of DB access"
    This reverts commit 00457d5623582cec8c680110d59c116ccd9ca1c9.

--- application/config/database.php
+++ application/config/database.php
@@ -37,18 +37,18 @@
 $active_group = "default";
 $active_record = TRUE;
 
-// $db['default']['hostname'] = "localhost";
-// $db['default']['username'] = "root";
-// $db['default']['password'] = "root";
-// $db['default']['database'] = "ci_login";
-// $db['default']['dbdriver'] = "mysql";
-// $db['default']['dbprefix'] = "";
-// $db['default']['pconnect'] = TRUE;
-// $db['default']['db_debug'] = TRUE;
-// $db['default']['cache_on'] = FALSE;
-// $db['default']['cachedir'] = "";
-// $db['default']['char_set'] = "utf8";
-// $db['default']['dbcollat'] = "utf8_general_ci";
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = "root";
+$db['default']['password'] = "root";
+$db['default']['database'] = "ci_login";
+$db['default']['dbdriver'] = "mysql";
+$db['default']['dbprefix'] = "";
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = "";
+$db['default']['char_set'] = "utf8";
+$db['default']['dbcollat'] = "utf8_general_ci";
 
 
 /* End of file database.php */
