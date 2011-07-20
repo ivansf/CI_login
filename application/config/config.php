a1b00b9551adc9c969df6ad2e01deb2a9f29a6cf
ivansf <ivansotof@gmail.com>
2011-07-19 18:51:24 -0600

    test

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://cilogin.local/";
+//$config['base_url']	= "http://cilogin.local/";
 
 /*
 |--------------------------------------------------------------------------
